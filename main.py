92ddfdfe7888e14c6810ccda3f4bccfdb626cfa4
Pavel Shlyak <shlyak-pavel2011@ya.ru>
2018-02-17 23:55:26 +0300

    Some fixes

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ from download import *
 from analyze import trainAnger
 from analyze import format_sentence
 from porter import Porter
+from os import system
 
 #Download messages
 SLEEP_TIME = 0.3
-all_history = get_history(friends, SLEEP_TIME)
+#all_history = get_history(friends, SLEEP_TIME)
+#Prepare train data
+os.system("./build_data.sh")
 #Train
 classifier=trainAnger()
 #Count everything
@@ -16,9 +19,10 @@ for message in all_history:
         if classifier.classify(format_sentence(message['body'])) == "neg":
             neg = neg + 1
         else:
+            print(message['body'])
             pos = pos + 1
 #Output
-print("You are positive by " + str(pos/(neg+pos)) + " percent")
+print("You are romantic by " + str(pos/(neg+pos)) + " percent")
 save_to_file(all_history, 'raw.txt')
 self_messages = get_messages_for_user(all_history, SELF_ID)
 save_to_file(self_messages, 'sm_corpus.txt')
